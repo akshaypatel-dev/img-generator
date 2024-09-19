3580f1df884b3cd36edee248992820860cc6d2aa
akshaypatel-dev <akshaypatel989@gmail.com>
2024-09-20 00:40:01 +0100

    mongoose updated

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -24,7 +24,7 @@ export const connectToDatabase = async () => {
 	cached.promise =
 		cached.promise ||
 		mongoose.connect(MONGODB_URL, {
-			dbName: "imagegenerator.images",
+			dbName: "imagegenerator",
 			bufferCommands: false,
 		});
 
